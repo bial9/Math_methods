4e34b738fb683cd16c03558eb40414d05d960ce8
Bilal <99763904+bial9@users.noreply.github.com>
2022-04-25 03:24:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+// var num = 3.45214
+// document.write(num+"<br>")
+// var a = Math.round(num)
+// document.write(a+"<br>")
+// var b = Math.floor(num)
+// document.write(b+"<br>")
+// var c = Math.ceil(num)
+// document.write(c)
+
+// var num = -2.673
+// document.write(num+"<br>")
+// var a = Math.round(num)
+// document.write(a+"<br>")
+// var b = Math.floor(num)
+// document.write(b+"<br>")
+// var c = Math.ceil(num)
+// document.write(c)
+
+// var a = Math.abs(-4)
+// document.write(a+"<br>")
+
+// var b = Math.abs(5)
+// document.write(b)
+
+// var a = Math.floor(Math.random())+4
+// document.write(a+"<br>")
+
+// var b = Math.floor(Math.random())+6
+// document.write(b)
+
+// var a = Math.floor(Math.random()*2)+1
+// document.write(a+"<br>")
+// if(a===1){
+//     document.write("Heads")
+// }
+// if(a===2){
+//     document.write("Tails")
+// }
+
+// var a = Math.floor(Math.random()*100)+1;
+// document.write(a);
+
+// var a = Math.floor(Math.random()*10)+1;
+// var b = +prompt("Enter a num between 1 to 10")
+// if(a===b){
+//     document.write("Congratulation")
+// }
+
